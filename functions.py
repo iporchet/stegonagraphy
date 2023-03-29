92679acad55204efdb8f35959a8b98b22ff71a5d
iporchet <iporchet25@gmail.com>
2023-03-28 22:37:08 -0700

    modified:   __pycache__/functions.cpython-310.pyc 	modified:   functions.py 	modified:   main.py

--- functions.py
+++ functions.py
@@ -2,11 +2,12 @@ def encode_bin(text):
     """Encondes binary message"""
     charsintext = [x for x in text]
     binary_codes = [bin(ord(character))[2:] for character in charsintext]
-    
     binary_codes.append(bin(ord(';'))[2:])
+
+    print(binary_codes)
+
     
     return binary_codes
-
 
 
 def decode_bin(array_of_bin):
@@ -15,6 +16,7 @@ def decode_bin(array_of_bin):
     characters = [chr(x) for x in ascii_codes]
 
     return ''.join(characters)
+
 
 def encode_message(data_array, bin_code):
 
@@ -30,7 +32,34 @@ def encode_message(data_array, bin_code):
                     if int(bin_code[code_indx]) != (data_array[x][y][value]%2):
                         data_array[x][y][value] -= 1
                         code_indx += 1
-
+                    
                     else:
                         code_indx += 1
         break
+
+    return data_array
+
+
+def extract_message(data_array):
+    """Function returns a list of binary codes"""
+    binary_array = []
+    bit_string = ""
+
+    for x in range(0, len(data_array)):
+        for y in range(0, len(data_array[x])):
+            for value in data_array[x][y]:
+                print(data_array[x][y])
+                if bin(ord(';'))[2:] in binary_array:
+                    break
+
+                if len(bit_string) == 7:
+                    binary_array.append(bit_string)
+                    bit_string = ""
+
+                else:
+                    bit_string += str(value%2)
+
+            break
+
+
+    return binary_array
--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import os
 import functions
 import numpy as np
+import copy
 import PIL as pl
 from PIL import Image
     
 image = Image.open('python/stegonagraphy/picutre-4.png') #Desired image path here
+
+# convert image into data array 
 data = np.asarray(image)
 
-# code to get ascii chracters into binary
+# create copy of data array to compare
+data0 = copy.deepcopy(data)
+
+# code to get ascii chracters into binary as string
 code = ''.join(functions.encode_bin("Hello World!"))
 
 functions.encode_message(data, code)
-
-print(data)
 
 
 if os.path.exists('python/stegonagraphy/new-picture.png') == False:
     new_img = Image.fromarray(data)
     new_img.save("new-picture.png")
+
+
+image1 = Image.open('new-picture.png') #Desired image path here
+data1 = np.asarray(image1)
+
+
+print("\n")
+functions.extract_message(data1)
+
